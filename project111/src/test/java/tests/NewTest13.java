165dc5aa63300c68f252347c821c6deb887e37b6
kishore <skill2automate@gmail.com>
2025-09-06 10:56:49 +0530

    added three cases

---
+++ project111/src/test/java/tests/NewTest13.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class NewTest13 {
+  @Test(enabled=false, description="login test with valid")
+  public void test1() {
+	  System.out.println("test1");
+  }
+  
+  @Test(timeOut=2000,expectedExceptions=ArithmeticException.class)
+  public void test2() throws Exception {
+	  System.out.println("test2");
+	  int a=45/0;
+  }
+  
+  public void test3() {
+	  System.out.println("test3");
+  }
+}
